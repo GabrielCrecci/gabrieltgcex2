3082cc0b76ee763cf8a6fa734a563cac043347f7
gabri <gabri@DESKTOP-GABRIEL>
2022-05-12 20:29:01 -0300

    exercício 1

---
+++ gabrieltgcex2/src/gabrieltgcex2/Gabrieltgcex2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gabrieltgcex2;
+
+/**
+ *
+ * @author gabri
+ */
+public class Gabrieltgcex2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
